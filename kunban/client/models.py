c1d1a2d514306eabc25a1780e4e5bdc7b85c26fe
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2024-05-22 20:00:19 +0300

    Add files via upload

--- kunban/client/models.py
+++ kunban/client/models.py
@@ -1,79 +1,78 @@
-from sqlalchemy import create_engine, MetaData, Column, Table, Integer, String, Boolean, ForeignKey, Float, \
+from sqlalchemy import create_engine, Column, Table, Integer, String, Boolean, ForeignKey, Float, \
     DateTime, func
-
-metadata = MetaData()
-
-engine = create_engine("postgresql+psycopg2://postgres:1234567890@localhost:5432/postgres", echo=True, pool_pre_ping=True)
-
-user = Table(
-    'users', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('login', String(length=60), nullable=False),
-    Column('password_hash', String(length=60), nullable=False),
-    Column('email', String(length=160), nullable=False),
-    Column('created_at', DateTime(timezone=True), default=func.now()),
-    Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
-    Column('is_admin', Boolean, default=False, nullable=False),
-)
+from sqlalchemy.orm import Session, declarative_base
 
 
-board = Table(
-    'board',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('title', String(length=120)),
-    Column('created_at', DateTime(timezone=True), default=func.now()),
-    Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
-    Column('author_id', Integer, ForeignKey(user.c.id))
-)
+engine = create_engine("postgresql+psycopg2://postgres:1234567890@localhost:5432/postgres", echo=True,)
+Base = declarative_base()
 
 
-column = Table(
-    'column',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('title', String(length=120)),
-    Column('created_at', DateTime(timezone=True), default=func.now()),
-    Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
-    Column('board_id', Integer, ForeignKey(board.c.id))
-)
+class UserModel(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(length=60), nullable=False)
+    password_hash = Column(String(length=60), nullable=False) 
+    email = Column( String(length=160), nullable=False)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_admin = Column(Boolean, default=False, nullable=False)
 
 
-ticket = Table(
-    'ticket',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('title', String(length=120)),
-    Column('created_at', DateTime(timezone=True), default=func.now()),
-    Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
-    Column('column_id', Integer, ForeignKey(column.c.id)),
-    Column('description', String, nullable=False),
-    Column('author_id', Integer, ForeignKey(user.c.id)),
-    Column('deadline', String, nullable=True),
-    Column('estimate', Float, nullable=True),
-    Column('priority', String, nullable=True),
-    Column('performer_id', Integer, ForeignKey(user.c.id))
-)
+class BoardModel(Base):
+    __tablename__ = 'board'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(length=120))
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    author_id = Column(ForeignKey('user.c.id'))
 
 
-others_users = Table(
-    'others_users',
-    metadata,
-    Column('user_id', Integer, ForeignKey(user.c.id)),
-    Column('board_id', Integer, ForeignKey(board.c.id)),
-)
+class ColumnModel(Base):
+    __tablename__ = 'column'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(length=120))
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    board_id = Column(ForeignKey('board.c.id'))    
 
 
-comment = Table(
-    'comment',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('created_at', DateTime(timezone=True), default=func.now()),
-    Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
-    Column('ticket_id', Integer, ForeignKey(ticket.c.id)),
-    Column('author_id', Integer, ForeignKey(user.c.id)),
-    Column('content', String, nullable=False)
-)
+class TicketModel(Base):
+    __tablename__ = 'ticket'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(length=120))
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    column_id = Column(ForeignKey('column.c.id'))
+    description = Column(String, nullable=False)
+    author_id = Column(ForeignKey('user.c.id'))
+    deadline = Column(String, nullable=True)
+    estimate = Column(Float, nullable=True)
+    priority = Column(String, nullable=True)
+    performer_id = Column(ForeignKey('user.c.id'))
 
 
-metadata.create_all(engine)
+class OtherUsersModel(Base):
+    __tablename__ = 'others_users'
+    user_id = Column(ForeignKey('user.c.id'))
+    board_id = Column(ForeignKey('board.c.id'))
+
+
+class CommentModel(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    ticket_id = Column(ForeignKey('ticket.c.id'))
+    author_id = Column(ForeignKey('user.c.id'))
+    content = Column(String, nullable=False)
+
+# with Session(engine) as session:
+#     with session.begin():
+#         UserModel.metadata.create_all(engine)
+#         user = UserModel(user_id=1, name='Jack', fullname='Jack Cow')
+#         session.add(user)
+
+
+#Base.create_all(engine)
+
+print(engine)
