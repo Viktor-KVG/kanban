063b15a9e2f4d5d8c3999d0ef6c749e3564c72d9
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2024-09-18 23:44:20 +0300

    Add files via upload

--- tests/test_general.py
+++ tests/test_general.py
@@ -9,11 +9,6 @@ client = TestClient(app)
 
 
 
-
-
-
-
-
 def test_create_user(connect_to_database):
     fake_user = {
                 "login": "string",
@@ -31,3 +26,29 @@ def test_create_user(connect_to_database):
     assert response.status_code != 400, f"Expected 200, got {response.status_code}. Response: {resp_json}"
     assert "token" in resp_json, "Expected key 'token' in the response."
     assert added_user is not None
+
+
+def test_search_users_list(connect_to_database):
+
+    response = client.get("/api/user/list", params={"login": 'string'})
+    resp_json = response.json() 
+
+    response_2 = client.get("/api/user/list", params={"email": "string"})
+    resp_json_2 = response_2.json()
+
+    assert response.status_code == 200, f"Expected 200, got {response.status_code}. Response: {resp_json}"
+    assert response_2.status_code == 200, f"Expected 200, got {response_2.status_code}. Response: {resp_json_2}"
+    assert resp_json[0]['id'] == 1
+    assert resp_json[0]['login'] == 'string'
+    assert resp_json[0]['email'] == 'string'
+
+
+def test_earch_user_id(connect_to_database):
+    response = client.get(f'/api/user/{1}')
+    resp_json = response.json()
+    assert response.status_code == 200, f"Expected 200, got {response.status_code}. Response: {resp_json}"
+
+    response_second = client.get(f'/api/user/{12}')
+    resp_json_second = response_second.json()
+    assert response_second.status_code == 400, f"Expected 400, got {response_second.status_code}. Response: {resp_json_second}"
+
