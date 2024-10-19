06e364aedc9df45f7552588af4877813e8096c75
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2024-10-19 19:48:39 +0300

    Add files via upload

--- src/routers/general_board.py
+++ src/routers/general_board.py
@@ -1,4 +1,101 @@
 from fastapi import APIRouter
 from src import core
 from src.auth import auth_jwt
-from src.schemas import (Token,)
+from src.schemas import (Token, 
+                         BoardsModel, 
+                         CreateBoardModel,
+                         BoardId, 
+                         PutBoard, 
+                         DeleteBoard)
+from fastapi import APIRouter, Depends, Query,  status, HTTPException
+
+
+api_router = APIRouter(
+    prefix="/api",
+    tags=["api"]
+)
+
+@api_router.post("/board", response_model=BoardsModel)   
+def create_board_model(data: CreateBoardModel):
+    if core.is_board_exist(data):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Error in data entry, such board already exists'
+        )
+
+    else:
+        board = core.create_board(data)
+        return board
+    
+
+@api_router.get('board/list')
+def boards_list(bard_title: str = None):
+    boadrs = core.boards_list(PutBoard(title=bard_title))
+    if boadrs:
+        return boadrs
+    
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Board not found'
+            )
+
+
+@api_router.get('board/{board_id}')
+def search_board_id(id_number: int):
+    if id_number <= 0:
+        raise HTTPException(
+            status_code=status.HTTP_417_EXPECTATION_FAILED,
+            detail='Board ID must not be zero'
+        )
+    
+    id_board = core.board_id_number(BoardId(id=id_number))
+
+    if id_board:
+        return id_board
+    
+    if id_board == False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid id number '
+        )     
+    
+
+@api_router.put('/board/{board_id}', response_model=BoardsModel)
+def put_board_id(board_id: int, board_update: PutBoard):
+    if board_id <= 0:
+        raise HTTPException(
+            status_code=status.HTTP_417_EXPECTATION_FAILED,
+            detail='Board ID must not be zero'
+        )    
+
+    board_put = core.serch_board_for_put(board_update, board_id)
+
+    if board_put:
+        return board_put
+    
+    if board_put == False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid board '
+        )
+    
+
+@api_router.delete('/board/{user_id}')
+def delete_board_id(board_id: int):
+    if board_id <= 0:
+        raise HTTPException(
+            status_code=status.HTTP_417_EXPECTATION_FAILED,
+            detail='Board ID must not be zero'
+        )    
+
+    board_del = core.search_board_for_delete(BoardId(id=board_id))
+
+    if board_del:
+        return board_del
+    
+    if board_del == False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid board '
+        )   
