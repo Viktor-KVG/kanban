e1064f5d8f7eb04b9b71729969579a0bd587fc28
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2024-11-26 23:59:06 +0300

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+
 from fastapi.testclient import TestClient
 import pytest
 from sqlalchemy import create_engine, text
@@ -9,6 +10,10 @@ from src.database import get_db
 from src.models import UserModel, BoardModel
 import datetime
 from src.database import session_factory
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 TEST_SQL_DATABASE_URL = 'postgresql+psycopg2://postgres:1234567890t@172.24.0.1:7000/postgres_test'
 
@@ -20,6 +25,7 @@ timestamp_value = datetime.datetime.now()
 
 def override_get_db():
     print("Using test database!++++++++++++++++++++++++")
+    logger.info(f"Используемая база данных: {TEST_SQL_DATABASE_URL}")
 
     db = test_session_factory()
     try:
@@ -55,21 +61,8 @@ def create_test_user(session):
     return fake_user    
 
 
-def clear_database(session):
-    # Получаем список таблиц
-    '''
-    - 'test_Base.metadata.sorted_tables':
-    - 'metadata' — это объект SQLAlchemy, который содержит информацию о таблицах, связанных с базой данных.
-    - 'sorted_tables' — это список всех таблиц, определенных в модели базы данных. 
-       Он отсортирован в порядке, установленном в классах моделей.
-
-    - Функция 'reversed()' возвращает итератор, который перебирает список в обратном порядке. 
-      Это может быть полезно, если некоторые таблицы имеют зависимости (например, внешние ключи), и вы хотите удалять их в порядке, 
-      противоположном тому, как они были добавлены, чтобы избежать ошибок ссылочной целостности.
-    '''
-       
+def clear_database(session):      
     print("Clearing the database...-----------")
-
     # Сначала удаляем из дочерних таблиц
     with test_session_factory() as session:
         print("Before Clearing: ", session.query(UserModel).count(), " users found initial.")
@@ -113,20 +106,6 @@ def test_client():
         yield client
 
     app.dependency_overrides[get_db] = lambda: None 
-
-
-# @pytest.fixture()
-# def session_scope():
-#     """Provide a transactional scope around a series of operations."""
-#     session = test_session_factory()
-#     try:
-#         yield session
-#         session.commit()
-#     except Exception:
-#         session.rollback()
-#         raise
-#     finally:
-#         session.close()
 
 
 
